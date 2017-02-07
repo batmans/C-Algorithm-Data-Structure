31659aa6e1b2c837aa55e998498e4648c1eefc90
batman <sadeghi.bah@gmail.com>
2017-02-06 18:45:50 -0800

    Lab 3
    Implementing and testing
    Stack and Queue

---
+++ lab3/lab3/src/Queue.h
@@ -0,0 +1,80 @@
+/*
+ * Queue.h
+ *
+ *  Created on: Feb 5, 2017
+ *      Author: bahman
+ */
+
+#ifndef QUEUE_H_
+#define QUEUE_H_
+
+#include<string>
+#include <iostream>
+#include <cstddef>
+using namespace std;
+
+class Queue
+{
+    public:
+        /**constructors and destructors*/
+
+        Queue();
+        //initializes an empty queue
+        //postcondition: an empty queue
+
+        Queue(const Queue &queue);
+        //initializes queue to have same elements as another queue
+        //postcondition: a copy of queue
+
+        ~Queue();
+        //frees memory allocated to the queue
+        //postcondition: memory for queue has been freed
+
+        /**manipulation procedures*/
+
+        void dequeue();
+        //removes an element from the front of the queue
+        //precondition: the queue isn't empty
+        //postcondition: an element has been removed from the front of the queue
+
+        void enqueue(string data);
+        //adds an element to the end of the queue
+        //postcondition: an element added to the end of the queue
+
+        /**accessors*/
+
+        bool operator==(const Queue &queue);
+        //returns whether this queue is equal to another queue
+
+        string get_front();
+        //returns the element at the front of the queue
+        //precondition: the queue is not empty
+
+        int get_size();
+        //returns the size of the queue
+
+        bool is_empty();
+        //returns whether the queue is empty
+
+        /**additional queue operations*/
+        void print();
+        //prints the elements in the queue in a programmer-specified format to stdout
+
+
+    private:
+        struct Node
+        {
+            string data;
+            Node* next;
+            Node(): data(""),next(NULL) {}
+            Node(string data):data(data),next(NULL){}
+        };
+
+        typedef struct Node* NodePtr;
+
+        NodePtr front;
+        NodePtr end;
+        int size;
+};
+
+#endif /* QUEUE_H_ */
